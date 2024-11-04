a8d8780c7bd556c988e780ec0fb30ea94b61b45c
flownumber <flownumber458@gmail.com>
2024-11-04 23:21:22 +0100

    daje

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import HomePage from './pages/HomePage';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './pages/Login'; // Importa il Login
+import Register from './pages/Register'; // Importa la Registrazione
 import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext'; // Importa il provider di autenticazione
 import './App.css';
 import Backend from './pages/backend';
 
+
 function App() {
   return (
-    <AuthProvider>
+    <AuthProvider> {/* Aggiungi AuthProvider qui */}
       <CartProvider>
-        <Router basename="/latteducello">
+        <Router>
           <div className="App">
             <Header />
             <Routes>
-              {/* Reindirizza direttamente alla HomePage */}
-              <Route path="/" element={<Navigate to="/homepage" replace />} />
-              <Route path="/homepage" element={<HomePage />} />
+              <Route path="/" element={<HomePage />} />
               <Route path="/product/:id" element={<ProductDetail />} />
               <Route path="/cart" element={<Cart />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/backend" element={<Backend />} />
-              {/* Redirect per rotte non definite */}
-              <Route path="*" element={<Navigate to="/homepage" />} />
+              <Route path="/login" element={<Login />} /> {/* Rotta per il login */}
+              <Route path="/register" element={<Register />} /> {/* Rotta per la registrazione */}
+              <Route path="/backend" element={<Backend />} /> 
             </Routes>
           </div>
         </Router>
