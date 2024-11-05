c0dc0e6512f70d44d2dc99bba941c44e2911f224
flownumber <flownumber458@gmail.com>
2024-11-05 14:38:55 +0100

    daje3

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import HomePage from './pages/HomePage';
 import Footer from './pages/Footer';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
-import Login from './pages/Login'; // Importa il Login
-import Register from './pages/Register'; // Importa la Registrazione
+import Login from './pages/Login';
+import Register from './pages/Register';
 import { CartProvider } from './context/CartContext';
-import { AuthProvider } from './context/AuthContext'; // Importa il provider di autenticazione
+import { AuthProvider } from './context/AuthContext';
 import './App.css';
 import Backend from './pages/backend';
 
-
 function App() {
   return (
-    <AuthProvider> {/* Aggiungi AuthProvider qui */}
+    <AuthProvider>
       <CartProvider>
-        <Router>
+        <Router basename="/latteducello"> {/* Inserisci il nome del repository qui */}
           <div className="App">
             <Header />
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/product/:id" element={<ProductDetail />} />
               <Route path="/cart" element={<Cart />} />
-              <Route path="/login" element={<Login />} /> {/* Rotta per il login */}
-              <Route path="/register" element={<Register />} /> {/* Rotta per la registrazione */}
-              <Route path="/backend" element={<Backend />} /> 
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/backend" element={<Backend />} />
             </Routes>
             <Footer />
           </div>
